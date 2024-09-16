de295bdc86009b14916e45dbfd931e13381d58fe
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-17 00:40:31 +0400

    update footer

--- src/components/contactComponents/Contact.tsx
+++ src/components/contactComponents/Contact.tsx
@@ -6,33 +6,47 @@ const Contact = () => {
     <footer className="max-w-[1440px] mx-auto px-[20px] md:px-[50px] flex justify-between">
       {/* footer title */}
       <div className="w-[300px] hidden md:block">
-        <p className="text-[16px] md:text-[20px] font-[900]">Ashtray</p>
+        <p className="text-[14px] md:text-[20px] font-[900] uppercase">Ashtray</p>
         <p className="text-[#AEAEAE]">
           Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit.
           Gravida massa volutpat aenean odio erat nullam fringilla.
         </p>
       </div>
 
+      {/* socials */}
       <SocialBlock />
 
-      <div>
-        <p className="text-[16px] md:text-[20px] font-[900] uppercase">quick links</p>
+      {/* quick links */}
+      <div className="flex flex-col gap-[5px]">
+        <p className="text-[14px] md:text-[20px] font-[900] uppercase">
+          quick links
+        </p>
         <div className="flex flex-col gap-[10px]">
-          {
-            menuArray.map((item, index)=>{
-              return(
-                <p className="text-[#AEAEAE] text-[14px] cursor-pointer hover:text-activeColor transition-all" key={index}>{item.name}</p>
-              )
-            })
-          }
+          {menuArray.map((item, index) => {
+            return (
+              <p
+                className="text-[#AEAEAE] text-[12px] md:text-[14px] cursor-pointer hover:text-activeColor transition-all"
+                key={index}
+              >
+                {item.name}
+              </p>
+            );
+          })}
         </div>
       </div>
 
-      <div>
-        <p className="text-[16px] md:text-[20px] font-[900] uppercase">Contact us</p>
+      {/* conatact */}
+      <div className="flex flex-col gap-[5px]">
+        <p className="text-[14px] md:text-[20px] font-[900] uppercase">
+          Contact us
+        </p>
         <div className="flex flex-col gap-[10px]">
-          <p className="text-[#AEAEAE]">yourinfo@gmail.com</p>
-          <p className="text-[#AEAEAE]">+995 000 00 00 00</p>
+          <p className="text-[#AEAEAE] text-[12px] md:text-[14px]">
+            info@ashtray.ge
+          </p>
+          <p className="text-[#AEAEAE] text-[12px] md:text-[14px]">
+            +995 000 00 00 00
+          </p>
         </div>
       </div>
     </footer>
--- src/components/contactComponents/SocialBlock.tsx
+++ src/components/contactComponents/SocialBlock.tsx
@@ -2,7 +2,7 @@ import { socialArray } from "../../lib/footerArrays";
 
 const SocialBlock = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-[5px]">
       <p className="text-[16px] md:text-[20px] font-[900] uppercase">Social</p>
       <div className="flex flex-col gap-[10px]">
         {socialArray.map((item, index) => {
@@ -15,7 +15,7 @@ const SocialBlock = () => {
               className="flex items-center gap-[10px] group social-item"
             >
               {item.icon}
-              <h4 className="text-[14px] text-[#AEAEAE] group-hover:text-activeColor cursor-pointer">
+              <h4 className="text-[12px] md:text-[14px] text-[#AEAEAE] group-hover:text-activeColor cursor-pointer">
                 {item.name}
               </h4>
             </a>
