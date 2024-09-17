7d7866aa5ec5bbe6626a531de68af5292bf1f7b0
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-17 11:00:04 +0400

    update navbar links

--- src/components/contactComponents/Contact.tsx
+++ src/components/contactComponents/Contact.tsx
@@ -2,11 +2,26 @@ import { menuArray } from "../../lib/menuArray";
 import SocialBlock from "./SocialBlock";
 
 const Contact = () => {
+
+  const handleClick = (event: React.MouseEvent, sectionId: string) => {
+    event.preventDefault();
+
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <footer id="contact" className="max-w-[1440px] mx-auto px-[20px] md:px-[50px] flex justify-between">
+    <footer
+      id="contact"
+      className="max-w-[1440px] mx-auto px-[20px] md:px-[50px] flex justify-between"
+    >
       {/* footer title */}
       <div className="w-[300px] hidden md:block">
-        <p className="text-[14px] md:text-[20px] font-[900] uppercase">Ashtray</p>
+        <p className="text-[14px] md:text-[20px] font-[900] uppercase">
+          Ashtray
+        </p>
         <p className="text-[#AEAEAE]">
           Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit.
           Gravida massa volutpat aenean odio erat nullam fringilla.
@@ -25,6 +40,7 @@ const Contact = () => {
           {menuArray.map((item, index) => {
             return (
               <p
+                onClick={(e)=>handleClick(e, item.sectionId)}
                 className="text-[#AEAEAE] text-[12px] md:text-[14px] cursor-pointer hover:text-activeColor transition-all"
                 key={index}
               >
